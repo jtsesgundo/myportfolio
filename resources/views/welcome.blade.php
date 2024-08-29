9dbee407f11aeceb7012708973e05152da4025b2
jtsesgndo <jtsesgundo@gmail.com>
2024-08-29 22:26:46 +0800

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,466 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Jeffrey Sesgundo</title>
+    <link rel="icon" type="image/x-icon" href="/img/favicon.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+  </head>
+  <body>
+
+
+    <header  id="home">
+      <div class="logo-holder">
+        <div><a href="#home" class="logo">JS</a></div>
+        <div><a href="#home" class="logo-text">Jeffrey Sesgundo</a></div>
+      </div>
+      <nav>
+        <ul id="menu">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#">Training, Awards & Certification</a>
+          </li>
+          <li>
+            <a href="mailto:jtsesgundo@gmail.com">Contact Me</a>
+          </li>
+        </ul>
+        <a href="#" class="mobile-toggle" onclick="toggleMobileMenu();"><i class="fa-solid fa-bars"></i></a>
+      </nav>
+      
+
+    </header>
+    <main>
+      <section class="hero container">
+        <div class="hero-left">
+          <h1><small>Hi! I am</small>
+            Jeffrey Sesgundo,
+            web developer.
+            <p>Frontend || Backend</p>
+          </h1>
+
+          <div class="call-to-action">
+              <a href="./img/My Resume.jpg" class="button" target="_blank">
+                View Resume
+              </a>
+          </div>
+          <div class="social-links">
+            <a href="https://github.com/jtsesgundo" target="_blank"><i class="fa-brands fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/jeffrey-sesgundo/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="https://www.facebook.com/jeffsesgundo/" target="_blank"><i class="fa-brands fa-facebook"></i></a>
+            <a href="mailto:jtsesgundo@gmail.com"><i class="fa-regular fa-envelope" target="_blank"></i></a>
+            <a href="https://www.facebook.com/messages/new" target="_blank"><i class="fa-brands fa-facebook-messenger" target="_blank"></i></a>
+            <a href=""><i class="fa-brands fa-skype" target="_blank"></i></a>
+          </div>
+        </div>
+        <div class="hero-right">
+          <img src="./img/pic.png" alt="Jeffrey Sesgundo" width="100%">
+        </div>
+      </section>
+
+      <section class="logos container">
+        <div class="marquee">
+          <div class="track">
+            <img src="./img/html.png" alt="HTML" width="80">
+            <img src="./img/css.png" alt="CSS" width="80">
+            <img src="./img/javascript.png" alt="Javascript" width="80">
+            <img src="./img/Bootstrap.png" alt="Bootstrap" width="80">
+            <img src="./img/php.png" alt="PHP" width="80">
+            <img src="./img/laravel.png" alt="Laravel" width="80">
+            <img src="./img/nodejs.png" alt="NODE.JS" width="80">
+            <img src="./img/github.jpg" alt="Github" width="80">
+            <img src="./img/mysql.png" alt="MYSQL" width="80">
+            <img src="./img/react.png" alt="React" width="80">
+            <img src="./img/vscode.png" alt="VSCODE" width="80">
+            <img src="./img/html.png" alt="HTML" width="80">
+            <img src="./img/css.png" alt="CSS" width="80">
+            <img src="./img/javascript.png" alt="Javascript" width="80">
+            <img src="./img/Bootstrap.png" alt="Bootstrap" width="80">
+            <img src="./img/php.png" alt="PHP" width="80">
+            <img src="./img/laravel.png" alt="Laravel" width="80">
+            <img src="./img/nodejs.png" alt="NODE.JS" width="80">
+            <img src="./img/github.jpg" alt="Github" width="80">
+            <img src="./img/mysql.png" alt="MYSQL" width="80">
+            <img src="./img/react.png" alt="React" width="80">
+            <img src="./img/vscode.png" alt="VSCODE" width="80">
+            <img src="./img/html.png" alt="HTML" width="80">
+            <img src="./img/css.png" alt="CSS" width="80">
+            <img src="./img/javascript.png" alt="Javascript" width="80">
+            <img src="./img/Bootstrap.png" alt="Bootstrap" width="80">
+            <img src="./img/php.png" alt="PHP" width="80">
+            <img src="./img/laravel.png" alt="Laravel" width="80">
+            <img src="./img/nodejs.png" alt="NODE.JS" width="80">
+            <img src="./img/github.jpg" alt="Github" width="80">
+            <img src="./img/mysql.png" alt="MYSQL" width="80">
+            <img src="./img/react.png" alt="React" width="80">
+            <img src="./img/vscode.png" alt="VSCODE" width="80">
+            <img src="./img/html.png" alt="HTML" width="80">
+            <img src="./img/css.png" alt="CSS" width="80">
+            <img src="./img/javascript.png" alt="Javascript" width="80">
+            <img src="./img/Bootstrap.png" alt="Bootstrap" width="80">
+            <img src="./img/php.png" alt="PHP" width="80">
+            <img src="./img/laravel.png" alt="Laravel" width="80">
+            <img src="./img/nodejs.png" alt="NODE.JS" width="80">
+            <img src="./img/github.jpg" alt="Github" width="80">
+            <img src="./img/mysql.png" alt="MYSQL" width="80">
+            <img src="./img/react.png" alt="React" width="80">
+            <img src="./img/vscode.png" alt="VSCODE" width="80">
+            <img src="./img/html.png" alt="HTML" width="80">
+            <img src="./img/css.png" alt="CSS" width="80">
+            <img src="./img/javascript.png" alt="Javascript" width="80">
+            <img src="./img/Bootstrap.png" alt="Bootstrap" width="80">
+            <img src="./img/php.png" alt="PHP" width="80">
+            <img src="./img/laravel.png" alt="Laravel" width="80">
+            <img src="./img/nodejs.png" alt="NODE.JS" width="80">
+            <img src="./img/github.jpg" alt="Github" width="80">
+            <img src="./img/mysql.png" alt="MYSQL" width="80">
+            <img src="./img/react.png" alt="React" width="80">
+            <img src="./img/vscode.png" alt="VSCODE" width="80">
+            <img src="./img/html.png" alt="HTML" width="80">
+            <img src="./img/css.png" alt="CSS" width="80">
+            <img src="./img/javascript.png" alt="Javascript" width="80">
+            <img src="./img/Bootstrap.png" alt="Bootstrap" width="80">
+            <img src="./img/php.png" alt="PHP" width="80">
+            <img src="./img/laravel.png" alt="Laravel" width="80">
+            <img src="./img/nodejs.png" alt="NODE.JS" width="80">
+            <img src="./img/github.jpg" alt="Github" width="80">
+            <img src="./img/mysql.png" alt="MYSQL" width="80">
+            <img src="./img/react.png" alt="React" width="80">
+            <img src="./img/vscode.png" alt="VSCODE" width="80">
+          </div>
+        </div>
+      </section>
+
+      <section class="skills container">
+        <h2>
+          <small>About Me</small>
+          Skills
+        </h2>
+        <div class="content">
+          <div class="left-content">
+            <div class="web-dev">
+              <h3>Web Development</h3>
+                <ul>
+                  <li><i class="fa-brands fa-html5"></i></li>
+                  <li><i class="fa-brands fa-css3"></i></li>
+                  <li><i class="fa-brands fa-js"></i></li>
+                  <li><i class="fa-brands fa-bootstrap"></i></li>
+                  <li><i class="fa-brands fa-react"></i></li>
+                  <li><i class="fa-brands fa-php"></i></li>
+                  <li><i class="fa-brands fa-laravel"></i></li>
+                  <li><i class="fa-brands fa-node-js"></i></li>
+                  <li><i class="fa-brands fa-github"></i></li>
+                </ul>
+            </div>
+            <div class="soft-skills"> 
+              <h3>Soft Skills</h3>
+                <div class="softSkills-holder">
+                  <div class="softSkills-left">
+                    <ul>
+                      <li><i class="fa-regular fa-star"></i>Good Communication</li>
+                      <li><i class="fa-regular fa-star"></i>Problem-Solving</li>
+                      <li><i class="fa-regular fa-star"></i>Team Collaboration</li>
+                      <li><i class="fa-regular fa-star"></i>Adaptability</li>
+                    </ul>
+                  </div>
+                  <div class="softSkills-right">
+                    <ul>
+                      <li><i class="fa-regular fa-star"></i>Eager to Learn</li>
+                      <li><i class="fa-regular fa-star"></i>Perseverance</li>
+                      <li><i class="fa-regular fa-star"></i>Organizational Skills</li>
+                      <li><i class="fa-regular fa-star"></i>Fast Learner</li>
+                    </ul>
+                  </div>
+                </div>
+            </div>
+          </div>
+          <div class="right-content">
+            <h3>Tools & Softwares</h3>
+              <div class="right-holder">
+                <div class="left-column">
+                  <div class="fb-ads">
+                    <h4>Facebook Advertising</h4>
+                    <ul>
+                      <li><i class="fa-solid fa-check"></i>Create and Launch Campaigns</li>
+                      <li><i class="fa-solid fa-check"></i>Conversion Objective</li>
+                      <li><i class="fa-solid fa-check"></i>Monitoring Metrics</li>
+                      <li><i class="fa-solid fa-check"></i>Scaling</li>
+                      <li><i class="fa-solid fa-check"></i>Retargeting Campaigns</li>
+                    </ul>
+                  </div>
+                  <div class="management-tools">
+                    <h4>Management Tools</h4>
+                    <ul>
+                      <li><img src="./img/trello.png" alt="Trello" width="40"> Trello</li>
+                      <li><img src="./img/slack.png" alt="Slack" width="40">  slack</li>
+                    </ul>
+                  </div>
+
+                  <div class="design">
+                    <h4>Designing</h4>
+                    <ul>
+                      <li><img src="./img/canva.png" alt="Canva" width="50"></li>
+                      <li><img src="./img/photoshop.png" alt="Photoshop" width="50"></li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="right-column">
+                  <div class="amazon">
+                    <h4>Amazon Seller Central Management</h4>
+                    <ul>
+                      <li><i class="fa-solid fa-check"></i>Product Research</li>
+                      <li><i class="fa-solid fa-check"></i>Supplier Sourcing</li>
+                      <li><i class="fa-solid fa-check"></i>Product Listing Optimization</li>
+                      <li><i class="fa-solid fa-check"></i>Amazon PPC</li>
+                    </ul>
+                  </div>
+                  <div class="microsoft">
+                    <h4>Microsoft</h4>
+                    <ul>
+                      <li><img src="./img/excel.png" alt="Excel" width="80"></li>
+                      <li><img src="./img/word.png" alt="Word" width="60"></li>
+                      <li><img src="./img/powerpoint.png" alt="Powerpoint" width="60"></li>
+                    </ul>
+                  </div>
+                  <div class="google">
+                    <h4>Google</h4>
+                    <ul>
+                      <li><img src="./img/sheets.png" alt="Sheets" width="100"></li>
+                      <li><img src="./img/docs.png" alt="Docs" width="80"></li>
+                      <li><img src="./img/slides.png" alt="Slides" width="90"></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="work-experience container">
+        <h2>
+          <small>Recent</small>
+          Work-Experience
+        </h2>
+        <div class="jobs">
+          <article>
+            <figure>
+              <div>
+                <img src="./img/kodego.png" alt="Kodego Bootcamp" width="100%">
+                <div class="caption">Kodego Bootcamp</div>
+              </div>
+            </figure>
+            <h3>Full Stack Web Developer</h3>
+            <div>March 26 - June 28, 2024</div>
+            <p>I recently completed an intensive 3-month bootcamp in KODEGO where I gained comprehensive training in full stack web development.
+              During my training, I developed a solid foundation in HTML, CSS, JavaScript, React, Node.js, and various other technologies. I have built several projects as part of my coursework, including a dynamic e-commerce website which demonstrate my ability to create responsive, user-friendly, and scalable web applications.</p>
+          </article>
+          <article>
+            <figure>
+              <div>
+                <img src="./img/jarir.jpg" alt="workplace " width="100%">
+                <div class="caption">Amazon VA(Remote)</div>
+              </div>
+            </figure>
+            <h3>Amazon Seller VA</h3>
+            <div>June - December 2021</div>
+            <h5>Duties & Responsibilities</h5>
+              <ul>
+                <li>Conducting market research to identify trending products and niche opportunities.</li>
+                <li>Analyzing competitor products and pricing strategies.</li>
+                <li>Contact & evaluating supplier options and negotiating terms.</li>
+                <li>Collaborating with the marketing team to promote products effectively.</li>
+              </ul>
+          </article>
+          <article>
+            <figure>
+              <div>
+                <img src="./img/eros1.jpg" alt="workplace 3" width="100%">
+                <div class="caption">UAE & KSA</div>
+              </div>
+            </figure>
+            <h3>Sales Officer</h3>
+            <div>2012 - 2021</div>
+            <h5>Duties & Responsibilities</h5>
+            <ul>
+              <li>Understanding product specifications and features to better sell them to customers.</li>
+              <li>Identifying customer needs and providing solutions through appropriate products or services.</li>
+              <li>Collaborating with team members to achieve sales goals.</li>
+              <li>Analyzing sales data to identify trends and improve sales strategies.</li>
+            </ul>
+          </article>
+        </div>
+      </section>
+
+      <div class="grid">
+        <article>
+          <h2>
+            <small>
+              Previous
+            </small>
+            Completed Projects
+          </h2>
+        </article>
+        <article>
+          <img src="./img/pawsitive.jpg" alt="Pawsitive">
+          <div>
+            <h3>Pawsitive</h3>
+            <p>KODEGO</p>
+          </div>
+        </article>
+        <article>
+          <img src="./img/time&watch.JPG" alt="Time & Watch">
+          <div>
+            <h3>Pawsitive</h3>
+            <p>KODEGO</p>
+          </div>
+        </article>
+        <article>
+          <img src="./img/crocs.JPG" alt="Crocs">
+          <div>
+            <h3>Pawsitive</h3>
+            <p>KODEGO</p>
+          </div>
+        </article>
+        <article>
+          <article>
+            <img src="./img/baguio.jpg" alt="Baguio City">
+          <div>
+            <h3>Pawsitive</h3>
+            <p>KODEGO</p>
+          </div>
+        </article>
+        <article>
+          <img src="./img/baguio.jpg" alt="Baguio City">
+        <div>
+          <h3>Pawsitive</h3>
+          <p>KODEGO</p>
+        </div>
+        </article>
+      </article>
+      <article>
+        <img src="./img/baguio.jpg" alt="Baguio City">
+      <div>
+        <h3>Pawsitive</h3>
+        <p>KODEGO</p>
+      </div>
+      </article>
+    </div>
+
+      {{-- <section class="projects">
+        <h2>
+          <small>
+            Previous
+          </small>
+          Completed Projects
+        </h2>
+        <div class="grid-container">
+          <article>
+            <div>
+              <img src="./img/pawsitive.jpg" alt="Pawsitive">
+            </div>
+              <h3>Pawsitive</h3>
+          </article>
+          <article>
+            <img src="./img/time&watch.JPG" alt="Time & Watch">
+            <div>
+              <h3>Time & Watch</h3>
+            </div>
+          </article>
+          <article>
+            <img src="./img/crocs.JPG" alt="Crocs">
+            <div>
+              <h3>Crocs</h3>
+            </div>
+          </article>
+          <article>
+            <img src="./img/baguio.jpg" alt="Baguio City">
+            <div>
+              <h3>Baguio City</h3>
+            </div>
+          </article>
+          <article>
+            <img src="./img/Calculator.JPG" alt="Calculator">
+            <div>
+              <h3>Calculator</h3>
+            </div>
+          </article>
+        <article>
+          <img src="./img/Converters.JPG" alt="Converters">
+          <div>
+            <h3>Converters</h3>
+          </div>
+        </article>
+        <article>
+          <img src="./img/Phases of life.JPG" alt="Phases of Life">
+          <div>
+            <h3>Phases of Life</h3>
+          </div>
+        </article>
+        <article>
+          <div>
+            <img src="./img/Rock, Scissors, Paper.JPG" alt="Rock, Scissors, Paper" width="100%">
+            <h3>Rock, Scissors, Paper</h3>
+
+
+          </div>
+        </article>
+        <article>
+          <img src="./img/To do, Search.JPG" alt="To do, Search">
+          <div>
+            <h3>To do, Search</h3>
+          </div>
+        </article>
+      </section> --}}
+
+      {{-- <section class="bento container" id="projects">
+        <h2>
+          <small>
+            Previous
+          </small>
+          Completed Projects
+        </h2>
+        <div class="bento-grid">
+          <div class="bento-item">
+            <a href="#"><img src="./img/pawsitive.jpg" alt="workplace 1" width="100%">
+            </a>
+          </div>
+          <div class="bento-item">
+            <a href="#"><img src="./img/crocs.jpg" alt="workplace 1" width="100%">
+            </a>
+          </div>
+          <div class="bento-item">
+            <a href="#"><img src="./img/time&watch.jpg" alt="workplace 1" width="100%">
+            </a>
+          </div>
+          <div class="bento-item"><a href="#"><img src="./img/baguio.jpg" alt="workplace 1" width="100%"></a>
+          </div>
+          <div class="bento-item"><a href="#"><img src="./img/baguio.jpg" alt="workplace 1" width="100%"></a>
+          </div>
+        </div>
+      </section> --}}
+
+
+    </main>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+  </body>
+  <script src="./script/script.js"></script>
+
+
+</html>
